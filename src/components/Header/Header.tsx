dd3cea5025078f89cf52999cff33f418323b1dee
Stoycho Donev <stoychod79@gmail.com>
2023-06-08 06:49:54 +0100

    feat: add a Header component with mui sign-in and cart buttons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,27 @@
 import "./Header.css";
+import Button from "@mui/material/Button";
+import Badge from "@mui/material/Badge";
+import IconButton from "@mui/material/IconButton";
+import AddShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 const Header = () => {
   return (
     <header>
-      <div className="logo">Eshop</div>
+      <div className="brand">Eshop</div>
+      <div>
+        <Button
+          sx={{ fontSize: "1.2rem" }}
+          aria-label="sign-in"
+          color="inherit"
+        >
+          Hello, sign-in
+        </Button>
+        <IconButton aria-label="access shopping cart" color="inherit">
+          <Badge badgeContent={2} color="primary">
+            <AddShoppingCartIcon sx={{ fontSize: "2rem" }} />
+          </Badge>
+        </IconButton>
+      </div>
     </header>
   );
 };
