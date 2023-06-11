733439e217a9edd686d4fb33b97cf302d65c074c
Stoycho Donev <stoychod79@gmail.com>
2023-06-11 20:00:30 +0100

    feat: add an Error page to react-router

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import Root from "./routes/Root/Root";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import ErrorPage from "./routes/Error/Error";
 import Register from "./routes/Register/Register";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/auth/register",
---
+++ src/routes/Error/Error.tsx
@@ -0,0 +1,33 @@
+import { useRouteError, isRouteErrorResponse } from "react-router-dom";
+import "./Error.css";
+
+const ErrorPage = () => {
+  // you don't need to explicitly set error to `unknown`
+  const error = useRouteError();
+  let errorMessage: string;
+  console.error(error);
+
+  if (isRouteErrorResponse(error)) {
+    // error is type `ErrorResponse`
+    errorMessage = error.error?.message || error.statusText;
+  } else if (error instanceof Error) {
+    errorMessage = error.message;
+  } else if (typeof error === "string") {
+    errorMessage = error;
+  } else {
+    console.error(error);
+    errorMessage = "Unknown error";
+  }
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{errorMessage}</i>
+      </p>
+    </div>
+  );
+};
+
+export default ErrorPage;
