5a652bf3d5ca854a292f22520d82edd87ce19c9d
Stoycho Donev <stoychod79@gmail.com>
2023-06-19 11:35:09 +0100

    feat: add user icon and menu in header if a user is logged in

---
+++ src/components/AccountMenu/AccountMenu.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import IconButton from "@mui/material/IconButton";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useLogoutUserMutation } from "../../api/apiSlice";
+import { useNavigate } from "react-router-dom";
+
+const AccountMenu = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const [logutUser] = useLogoutUserMutation();
+  const navigate = useNavigate();
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleLogout = async () => {
+    try {
+      await logutUser().unwrap();
+      navigate("/");
+      handleClose();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <IconButton
+        size="large"
+        aria-label="account of current user"
+        aria-controls="menu-appbar"
+        aria-haspopup="true"
+        onClick={handleMenu}
+        color="inherit"
+      >
+        <AccountCircle />
+      </IconButton>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+      </Menu>
+    </>
+  );
+};
+
+export default AccountMenu;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,17 +3,25 @@ import Button from "@mui/material/Button";
 import Badge from "@mui/material/Badge";
 import IconButton from "@mui/material/IconButton";
 import AddShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useNavigate } from "react-router-dom";
 import { useCheckAuthenticatedQuery } from "../../api/apiSlice";
+import AccountMenu from "../AccountMenu/AccountMenu";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   const { data: isAuthenticated } = useCheckAuthenticatedQuery();
   return (
     <header>
       <div className="brand">Eshop</div>
       <div>
-        <Button aria-label="sign-in-out" color="inherit">
-          {isAuthenticated ? "Sign out" : "Sign in"}
-        </Button>
+        {isAuthenticated ? (
+          <AccountMenu />
+        ) : (
+          <Button color="inherit" onClick={() => navigate("/auth/login")}>
+            Sign-in
+          </Button>
+        )}
         <IconButton aria-label="access shopping cart" color="inherit">
           <Badge badgeContent={2} color="primary">
             <AddShoppingCartIcon />
