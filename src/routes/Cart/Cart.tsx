546a783351edac0d9e5dae09c54864f2e6ee358a
Stoycho Donev <stoychod79@gmail.com>
2023-07-05 11:21:09 +0100

    refactor: hide action card, price and subtotal labels when basket is empty

--- src/routes/Cart/Cart.tsx
+++ src/routes/Cart/Cart.tsx
@@ -4,6 +4,7 @@ import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
 import { useGetCartQuery } from "../../api/apiSlice";
 import CartItem from "../CartItem/CartItem";
 
@@ -13,6 +14,9 @@ const Cart = () => {
 
   // Keep track of subtotal amount
   const [subTotal, setSubtotal] = useState(0);
+  const [cartEmpty, setCartEmpty] = useState(
+    cartItems && cartItems?.length < 1
+  );
 
   useEffect(() => {
     // calculate total price
@@ -21,10 +25,11 @@ const Cart = () => {
     }, 0);
 
     setSubtotal(total || 0);
+    setCartEmpty(cartItems && cartItems?.length < 1);
   }, [cartItems]);
 
   const renderCartItems = () => {
-    if (cartItems && cartItems?.length > 0) {
+    if (!cartEmpty) {
       const items = cartItems?.map((cartItem) => {
         return <CartItem key={cartItem.cartItemId} cartItem={cartItem} />;
       });
@@ -32,18 +37,64 @@ const Cart = () => {
     }
 
     return (
-      <Typography
-        variant="h5"
-        align="center"
-        component="h3"
-        sx={{
-          borderBottom: "1px solid gray",
-          paddingY: "6rem",
-        }}
-      >
-        Your basket is empty
-      </Typography>
+      <>
+        <Divider />
+        <Typography
+          variant="h5"
+          align="center"
+          component="h3"
+          sx={{
+            // borderBottom: "1px solid gray",
+            paddingY: "6rem",
+          }}
+        >
+          Your basket is empty
+        </Typography>
+      </>
     );
+  };
+
+  const renderActionCard = () => {
+    if (!cartEmpty) {
+      return (
+        <Paper
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            padding: "2rem",
+            marginTop: { xs: "1.5rem", md: 0 },
+            marginLeft: { md: "0.75rem", lg: "1.5rem" },
+          }}
+        >
+          <Typography component="div" gutterBottom variant="h6">
+            Subtotal ({cartItems?.length}{" "}
+            {cartItems?.length === 1 ? "item" : "items"}):{" "}
+            <Box fontWeight="bold" display="inline">
+              {subTotal.toLocaleString("en-GB", {
+                style: "currency",
+                currency: "GBP",
+              })}
+            </Box>
+          </Typography>
+          <Button
+            sx={{ marginTop: "2rem" }}
+            variant="contained"
+            href="/payment"
+          >
+            Proceed to ckeckout
+          </Button>
+          <Button
+            sx={{ marginTop: "2rem" }}
+            variant="contained"
+            color="secondary"
+            href="/products"
+          >
+            {" "}
+            Continue shopping
+          </Button>
+        </Paper>
+      );
+    }
   };
 
   return (
@@ -60,69 +111,46 @@ const Cart = () => {
             <Typography variant="h4" component="h1" paddingTop="1rem">
               Shopping basket
             </Typography>
-            <Typography
-              variant="h6"
-              component="h3"
-              fontWeight={400}
-              lineHeight={1.2}
-              align="right"
-              borderBottom="1px solid gray"
-              sx={{
-                display: { xs: "none", md: "block" },
-              }}
-            >
-              price
-            </Typography>
+            {!cartEmpty && (
+              <>
+                <Typography
+                  variant="h6"
+                  component="h3"
+                  fontWeight={400}
+                  lineHeight={1.2}
+                  align="right"
+                  // borderBottom="1px solid gray"
+                  sx={{
+                    display: { xs: "none", md: "block" },
+                  }}
+                >
+                  price
+                </Typography>
+                <Divider />
+              </>
+            )}
             {renderCartItems()}
-            <Typography
-              component="div"
-              gutterBottom
-              variant="h6"
-              align="right"
-              marginBottom="3rem"
-            >
-              Subtotal ({cartItems?.length}{" "}
-              {cartItems?.length === 1 ? "item" : "items"}):{" "}
-              <Box fontWeight="bold" display="inline">
-                {subTotal.toLocaleString("en-GB", {
-                  style: "currency",
-                  currency: "GBP",
-                })}
-              </Box>
-            </Typography>
+            {!cartEmpty && (
+              <Typography
+                component="div"
+                gutterBottom
+                variant="h6"
+                align="right"
+                marginBottom="3rem"
+                // borderTop="1px solid gray"
+              >
+                Subtotal ({cartItems?.length}{" "}
+                {cartItems?.length === 1 ? "item" : "items"}):{" "}
+                <Box fontWeight="bold" display="inline">
+                  {subTotal.toLocaleString("en-GB", {
+                    style: "currency",
+                    currency: "GBP",
+                  })}
+                </Box>
+              </Typography>
+            )}
           </Paper>
-          <Paper
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              padding: "2rem",
-              marginTop: { xs: "1.5rem", md: 0 },
-              marginLeft: { md: "0.75rem", lg: "1.5rem" },
-            }}
-          >
-            <Typography component="div" gutterBottom variant="h6">
-              Subtotal ({cartItems?.length}{" "}
-              {cartItems?.length === 1 ? "item" : "items"}):{" "}
-              <Box fontWeight="bold" display="inline">
-                {subTotal.toLocaleString("en-GB", {
-                  style: "currency",
-                  currency: "GBP",
-                })}
-              </Box>
-            </Typography>
-            <Button sx={{ marginTop: "2rem" }} variant="contained" href="/payment">
-              Proceed to ckeckout
-            </Button>
-            <Button
-              sx={{ marginTop: "2rem" }}
-              variant="contained"
-              color="secondary"
-              href="/products"
-            >
-              {" "}
-              Continue shopping
-            </Button>
-          </Paper>
+          {renderActionCard()}
         </Stack>
       </Box>
     </Box>
--- src/routes/CartItem/CartItem.tsx
+++ src/routes/CartItem/CartItem.tsx
@@ -9,6 +9,7 @@ import InputLabel from "@mui/material/InputLabel";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
 import {
   useUpdateCartItemMutation,
   useDeleteCartItemMutation,
@@ -41,92 +42,96 @@ const CartItem = ({ cartItem }: { cartItem: CartItem }) => {
   };
 
   return (
-    <Stack
-      sx={{
-        flexDirection: { md: "row" },
-        alignItems: { xs: "center", sm: "flex-start" },
-        paddingY: "1rem",
-        paddingLeft: { sm: "1rem" },
-        borderBottom: "1px solid gray",
-      }}
-    >
-      <Box component={RouterLink} to={`/products/${productId}`}>
-        <Box
-          component="img"
-          sx={{
-            width: { xs: 135, lg: 180 },
-            height: { xs: 135, lg: 180 },
-          }}
-          alt="product image"
-          src={image}
-        />
-      </Box>
+    <>
       <Stack
         sx={{
+          flexDirection: { md: "row" },
+          alignItems: { xs: "center", sm: "flex-start" },
+          paddingY: "1rem",
           paddingLeft: { sm: "1rem" },
-          flexGrow: 1,
+          // borderBottom: "1px solid gray",
         }}
       >
+        <Box component={RouterLink} to={`/products/${productId}`}>
+          <Box
+            component="img"
+            sx={{
+              width: { xs: 135, lg: 180 },
+              height: { xs: 135, lg: 180 },
+            }}
+            alt="product image"
+            src={image}
+          />
+        </Box>
         <Stack
           sx={{
-            flexDirection: { md: "row" },
-            alignItems: "",
+            paddingLeft: { sm: "1rem" },
+            flexGrow: 1,
           }}
         >
-          <Link
-            variant="h6"
-            color="inherit"
-            underline="none"
-            component={RouterLink}
-            to={`/products/${productId}`}
-          >
-            {name}
-          </Link>
-          <Typography
-            variant="h6"
-            fontWeight={700}
-            component="h3"
-            flexGrow={1}
+          <Stack
             sx={{
-              textAlign: { md: "right" },
-              marginLeft: "1rem",
+              flexDirection: { md: "row" },
+              alignItems: "",
             }}
           >
-            {(price / 100).toLocaleString("en-GB", {
-              style: "currency",
-              currency: "GBP",
-            })}
-          </Typography>
-        </Stack>
-        <FormControl sx={{ my: 4, minWidth: 120,
-          maxWidth: {sm: 220, md: 120}
-        }} size="small">
-          <InputLabel id="demo-simple-select-label">Quantity</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={currentQuantity}
-            label="Quantity"
-            onChange={handleQuantityChange}
+            <Link
+              variant="h6"
+              color="inherit"
+              underline="none"
+              component={RouterLink}
+              to={`/products/${productId}`}
+            >
+              {name}
+            </Link>
+            <Typography
+              variant="h6"
+              fontWeight={700}
+              component="h3"
+              flexGrow={1}
+              sx={{
+                textAlign: { md: "right" },
+                marginLeft: "1rem",
+              }}
+            >
+              {(price / 100).toLocaleString("en-GB", {
+                style: "currency",
+                currency: "GBP",
+              })}
+            </Typography>
+          </Stack>
+          <FormControl
+            sx={{ my: 4, minWidth: 120, maxWidth: { sm: 220, md: 120 } }}
+            size="small"
           >
-            <MenuItem value={1}>1</MenuItem>
-            <MenuItem value={2}>2</MenuItem>
-            <MenuItem value={3}>3</MenuItem>
-            <MenuItem value={4}>4</MenuItem>
-            <MenuItem value={5}>5</MenuItem>
-            <MenuItem value={6}>6</MenuItem>
-            <MenuItem value={7}>7</MenuItem>
-          </Select>
-        </FormControl>
-        <Button
-          variant="contained"
-          sx={{ my: 4, minWidth: 120, maxWidth: {sm: 220, md: 120 }, mt: 0 }}
-          onClick={() => deleteCartItem(cartItemId)}
-        >
-          Delete
-        </Button>
+            <InputLabel id="demo-simple-select-label">Quantity</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={currentQuantity}
+              label="Quantity"
+              onChange={handleQuantityChange}
+            >
+              <MenuItem value={1}>1</MenuItem>
+              <MenuItem value={2}>2</MenuItem>
+              <MenuItem value={3}>3</MenuItem>
+              <MenuItem value={4}>4</MenuItem>
+              <MenuItem value={5}>5</MenuItem>
+              <MenuItem value={6}>6</MenuItem>
+              <MenuItem value={7}>7</MenuItem>
+            </Select>
+          </FormControl>
+          <Button
+            variant="contained"
+            sx={{ my: 4, minWidth: 120, maxWidth: { sm: 220, md: 120 }, mt: 0 }}
+            onClick={() => deleteCartItem(cartItemId)}
+          >
+            Delete
+          </Button>
+        </Stack>
       </Stack>
-    </Stack>
+      <Divider />
+    </>
   );
 };
 
