94e370ca90c0c0c714c90890aa09fe833934d2ab
Stoycho Donev <stoychod79@gmail.com>
2023-07-06 15:58:52 +0100

    feat: add a link to orders page in completion page

--- src/routes/Completion/Completion.tsx
+++ src/routes/Completion/Completion.tsx
@@ -1,10 +1,30 @@
+import { Link as RouterLink } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
 
 const Completion = () => {
-  return <Box sx={{ marginX: "auto", marginTop: "5rem" }}>
-    <Typography variant="h4" component="h1">Thank you for shopping</Typography> 
-  </Box>;
+  return (
+    <Box sx={{ marginX: "auto", marginTop: "5rem" }}>
+      <Typography variant="h4" component="h1" sx={{
+        fontSize: {xs: "2rem", sm: "3rem"}
+      }}>
+        Thank you for shopping
+      </Typography>
+      <Link
+        variant="h6"
+        underline="none"
+        component={RouterLink}
+        to={"/orders"}
+        sx={{
+          display: "block",
+          textAlign: "center",
+        }}
+      >
+        {"Review your recent orders"}
+      </Link>
+    </Box>
+  );
 };
 
 export default Completion;
