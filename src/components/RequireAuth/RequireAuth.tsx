d85c708fb4344cf0646271a6bb8a49b6a05e3361
Stoycho Donev <stoychod79@gmail.com>
2023-07-07 14:28:36 +0100

    fix: fix false not authenticated condition in RequireAuth component
    wait for isAuthenticated to laod before rndering

--- src/components/RequireAuth/RequireAuth.tsx
+++ src/components/RequireAuth/RequireAuth.tsx
@@ -1,15 +1,38 @@
 import { useLocation, Navigate, Outlet } from "react-router-dom";
 import { useCheckAuthenticatedQuery } from "../../api/apiSlice";
+import LoadingScreen from "../LoadingScreen/LoadingScreen";
+import ErrorPage from "../ErrorPage/ErrorPage";
 
 const RequireAuth = () => {
-  const { data: isAuthenticated } = useCheckAuthenticatedQuery();
+  const {
+    data: isAuthenticated,
+    isFetching,
+    isSuccess,
+    isError,
+    error,
+  } = useCheckAuthenticatedQuery();
   const location = useLocation();
 
-  return isAuthenticated ? (
-    <Outlet />
-  ) : (
-    <Navigate to="/auth/login" state={{ from: location }} replace />
-  );
+  const renderComponent = () => {
+    if (isFetching) {
+      return <LoadingScreen />;
+    }
+
+    if (isError) {
+      console.log(error);
+      return <ErrorPage />;
+    }
+
+    if (isSuccess) {
+      if (isAuthenticated === true) {
+        return <Outlet />;
+      }
+
+      return <Navigate to="/auth/login" state={{ from: location }} replace />;
+    }
+  };
+
+  return renderComponent();
 };
 
 export default RequireAuth;
